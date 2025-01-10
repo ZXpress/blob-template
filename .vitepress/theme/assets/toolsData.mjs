f5952776e40d0a5da73eebcf16b0656a87ceb83a
Xpress <1832244088@qq.com>
2025-01-10 16:04:13 +0800

    commit

---
+++ .vitepress/theme/assets/toolsData.mjs
@@ -0,0 +1,358 @@
+const toolsData = [
+  {
+    type: "type1",
+    typeName: "技术博客1",
+    typeDesc: "技术知识为主的博客",
+    typeList: [
+      {
+        name: "张洪 Heo",
+        avatar: "https://p.zhheo.com/8DSTSS20990281646044689944.PNG!cover_240w",
+        desc: "产品设计师，独立开发者，设计与科技分享",
+        url: "https://blog.zhheo.com",
+      },
+      {
+        name: "IMSYY",
+        avatar: "https://blog.imsyy.top/images/logo/logo.webp",
+        desc: "分享技术与科技生活",
+        url: "https://blog.imsyy.top",
+      },
+      {
+        name: "EFU",
+        avatar: "https://s3.qjqq.cn/47/6625c1a86d7fd.webp!color",
+        desc: "分享技术与科技生活",
+        url: "https://www.efu.me",
+      },
+      {
+        name: "花森",
+        avatar: "http://n.huasenjio.top/img/favicon.57a83e22.svg",
+        desc: "前端网址导航博客",
+        url: "http://n.huasenjio.top/#/",
+      },
+      {
+        name: "isYangs",
+        avatar: "https://7.isyangs.cn/8/655c9835780a0-8.jpg",
+        desc: "一个前端Bug构造师的博客",
+        url: "https://isyangs.cn",
+      },
+    ],
+  },
+  {
+    type: "type1",
+    typeName: "技术博客2",
+    typeDesc: "技术知识为主的博客",
+    typeList: [
+      {
+        name: "Amaya丶夜雨",
+        avatar: "https://amayaliu.cn/images/favicon.jpg",
+        desc: "二次元, 小唯美",
+        url: "https://amayaliu.cn/",
+      },
+      {
+        name: "HaoUu",
+        avatar: "/images/logo/favicon-512x512.webp",
+        desc: "HaoYu的博客",
+        url: "https://g-haoyu.top/",
+      },
+      {
+        name: "从01开始",
+        avatar: "/images/logo/favicon-512x512.webp",
+        desc: "晓林的博客",
+        url: "https://www.peterjxl.com/",
+      },
+
+    ],
+  },
+  {
+    type: "type2",
+    typeName: "前端文档",
+    typeDesc: "All the project documents I have used",
+    typeList: [
+      {
+        name: "Hexo",
+        avatar: "https://hexo.io/logo.svg",
+        desc: "Hexo官方文档",
+        url: "https://hexo.io/zh-cn/",
+      },
+      {
+        name: "VitePress",
+        avatar: "https://vitepress.dev/vitepress-logo-mini.svg",
+        desc: "VitePress官方文档",
+        url: "https://vitepress.dev/zh/guide/getting-started",
+      },
+      {
+        name: "Element",
+        avatar: "https://element.eleme.cn/favicon.ico",
+        desc: "Element UI 官方文档",
+        url: "https://element.eleme.cn/#/zh-CN",
+      },
+      {
+        name: "Element PLUS",
+        avatar: "https://element-plus.org/images/element-plus-logo-small.svg",
+        desc: "Element PLUS 官方文档",
+        url: "https://element-plus.org/zh-CN/",
+      },
+      {
+        name: "Ant Design",
+        avatar: "https://next.antdv.com/assets/logo.1ef800a8.svg",
+        desc: "Ant Design Vue 官方文档",
+        url: "https://antdv.com/components/overview",
+      },
+      {
+        name: "AnimeJS",
+        avatar: "https://animejs.com/documentation/assets/img/favicon.png",
+        desc: "Anime.js是一个轻量级JavaScript动画库",
+        url: "https://animejs.com/",
+      },
+      {
+        name: "LodashJS",
+        avatar: "https://www.lodashjs.com/img/logo.png",
+        desc: "LodashJS 官方文档",
+        url: "https://www.lodashjs.com/",
+      },
+      {
+        name: "毛玻璃",
+        avatar: "https://hype4.academy/_next/static/media/logo.b6977cf7.svg",
+        desc: "GlassMorphism 毛玻璃效果",
+        url: "https://hype4.academy/tools/glassmorphism-generator",
+      },
+      {
+        name: "Echarts",
+        avatar: "https://echarts.apache.org/zh/images/favicon.png",
+        desc: "Echarts 官方文档",
+        url: "https://echarts.apache.org",
+      },
+      {
+        name: "Pug",
+        avatar: "https://www.pugjs.cn/img/logo.png",
+        desc: "Pug模板引擎中文官方文档",
+        url: "https://www.pugjs.cn/",
+      },
+      {
+        name: "NPM",
+        avatar: "https://static-production.npmjs.com/5f6e93af5bf0f5dcdd1eecdac99f51ee.png",
+        desc: "NPM 官网",
+        url: "https://www.npmjs.com/",
+      },
+      {
+        name: "NPMMirror",
+        avatar: "https://npmmirror.com/cnpm.png",
+        desc: "npm mirror 镜像站",
+        url: "https://npmmirror.com/",
+      },
+      {
+        name: "iconfont",
+        avatar: "https://img.alicdn.com/imgextra/i4/O1CN01Z5paLz1O0zuCC7osS_!!6000000001644-55-tps-83-82.svg",
+        desc: "阿里巴巴矢量图标库",
+        url: "https://www.iconfont.cn/",
+      },
+      {
+        name: "Icons8",
+        avatar: "https://goodies.icons8.com/web/common/header/logo/logo-icons8-christmas.svg?2025",
+        desc: "免费的图标、剪贴画插图、照片和音乐",
+        url: "https://igoutu.cn/",
+      },
+
+    ],
+  },
+  {
+    type: "type2",
+    typeName: "后端文档",
+    typeDesc: "All backend development needs",
+    typeList: [
+      {
+        name: "Maven",
+        avatar: "https://mvnrepository.com/assets/images/7080b8b0f6f48e6fbaffd5f9d85fcc7f-favicon.ico",
+        desc: "中央仓库",
+        url: "https://mvnrepository.com/",
+      },
+      {
+        name: "EasyExcel",
+        avatar: "https://easyexcel.opensource.alibaba.com/img/logo.png",
+        desc: "不用考虑性能、内存的等因素的情况下，快速完成Excel的读、写等功能",
+        url: "https://easyexcel.opensource.alibaba.com/",
+      },
+      {
+        name: "MyBatis-Plus",
+        avatar: "https://baomidou.com/assets/asset.cIbiVTt__ZgvyzK.svg",
+        desc: "在 MyBatis 的基础上只做增强不做改变",
+        url: "https://baomidou.com/getting-started/",
+      },
+      {
+        name: "藏经阁",
+        avatar: "https://img.alicdn.com/tfs/TB1_ZXuNcfpK1RjSZFOXXa6nFXa-32-32.ico",
+        desc: "阿里云开发者藏经阁",
+        url: "https://developer.aliyun.com/ebook/index/ebook-classify-3-ali__0_0_0_3",
+      },
+      {
+        name: "Arthas",
+        avatar: "https://arthas.aliyun.com/images/favicon.ico",
+        desc: "Alibaba开源的Java诊断工具",
+        url: "https://arthas.aliyun.com/doc/quick-start.html",
+      },
+      {
+        name: "一言",
+        avatar: "https://hitokoto.cn/favicon.ico?t=1527937218382",
+        desc: "一言官网",
+        url: "https://hitokoto.cn/",
+      },
+      {
+        name: "BaiDu",
+        avatar: "https://mapopen-website-wiki.cdn.bcebos.com/LOGO/lbsyunlogo_icon.ico",
+        desc: "百度地图开发平台",
+        url: "https://lbsyun.baidu.com/apiconsole/key#/home",
+      },
+    ],
+  },
+  {
+    type: "type3",
+    typeName: "AI",
+    typeDesc: "All backend development needs",
+    typeList: [
+      {
+        name: "ChatGPT",
+        avatar: "https://chatgpt.com/favicon.ico",
+        desc: "人类最强聊天AI",
+        url: "https://chatgpt.com/",
+      },
+      {
+        name: "LibLib",
+        avatar: "https://liblibai-web-static.vibrou.com/favicon.ico",
+        desc: "哩布哩布-AI创作平台",
+        url: "https://www.liblib.art/",
+      },
+      {
+        name: "文心一格",
+        avatar: "https://yige.baidu.com/favicon.png",
+        desc: "AI艺术和创意辅助平台",
+        url: "https://yige.baidu.com/",
+      },
+      {
+        name: "文心一言",
+        avatar: "https://liblibai-web-static.vibrou.com/favicon.ico",
+        desc: "文心一言",
+        url: "https://www.liblib.art/",
+      },
+      {
+        name: "TalkX",
+        avatar: "/images/logo/favicon-512x512.webp",
+        desc: "TalkX",
+        url: "https://web.talkx.cn/#/",
+      },
+    ],
+  },
+  {
+    type: "type4",
+    typeName: "Backup",
+    typeDesc: "",
+    typeList: [
+      {
+        name: "IStoreOS",
+        avatar: "https://doc.linkease.com/favicon.ico",
+        desc: "IStoreOS 官方文档",
+        url: "https://doc.linkease.com/zh/guide/istoreos",
+      },
+      {
+        name: "DrawIO",
+        avatar: "https://app.diagrams.net/favicon.ico",
+        desc: "画流程图的网站",
+        url: "https://app.diagrams.net/",
+      },
+      {
+        name: "极速空间",
+        avatar: "https://www.365pcbuy.com/static/img/favicon.ico",
+        desc: "极速空间, 显卡天梯图",
+        url: "https://www.365pcbuy.com/article-398.html",
+      },
+      {
+        name: "火币",
+        avatar: "https://hbg-fed-static-prd.hbfile.net/enmarket/favicon.ico?exchange",
+        desc: "火币官网",
+        url: "https://www.htx.com/zh-cn/trade/btc_usdt/",
+      },
+      {
+        name: "SMS",
+        avatar: "https://smsactivate.s3.eu-central-1.amazonaws.com/assets/img/activate_favicon.png",
+        desc: "俄罗斯手机号购买平台",
+        url: "https://sms-activate.io/cn",
+      },
+      {
+        name: "OSChina",
+        avatar: "https://static.oschina.net/new-osc/img/favicon.ico",
+        desc: "中文开源技术交流社区",
+        url: "https://www.oschina.net/",
+      },
+      {
+        name: "域名查询",
+        avatar: "https://beian.miit.gov.cn/img/bg_logo.2ce2f33d.png",
+        desc: "工业和信息化部, 域名信息备案管理系统",
+        url: "https://beian.miit.gov.cn/#/Integrated/index",
+      },
+    ],
+  },
+  {
+    type: "type5",
+    typeName: "Github",
+    typeDesc: "Github项目",
+    typeList: [
+      {
+        name: "张洪 Heo",
+        avatar: "https://p.zhheo.com/8DSTSS20990281646044689944.PNG!cover_240w",
+        desc: "产品设计师，独立开发者，设计与科技分享",
+        url: "https://blog.zhheo.com",
+      },
+    ],
+  },
+  {
+    type: "type500",
+    typeName: "网址导航",
+    typeDesc: "网址导航",
+    typeList: [
+      {
+        name: "集知盒子",
+        avatar: "/images/logo/favicon-512x512.webp",
+        desc: "各种资源网址导航",
+        url: "https://www.jizhihezi.com/",
+      },
+      {
+        name: "MikuTools",
+        avatar: "/images/logo/favicon-512x512.webp",
+        desc: "许多小工具的集合",
+        url: "https://tools.miku.ac/",
+      },
+      {
+        name: "OldMan",
+        avatar: "/images/logo/favicon-512x512.webp",
+        desc: "老男孩-掌机游戏下载",
+        url: "https://www.oldmantvg.net/",
+      },
+    ],
+  },
+  {
+    type: "rec",
+    typeName: "推荐",
+    typeDesc: "都是大佬，推荐关注",
+    typeList: [
+      {
+        name: "阮一峰",
+        avatar: "https://pic.efefee.cn/uploads/2024/02/26/65dc5fb729cdb.webp",
+        desc: "阮老师，知名博主，大佬中的大佬",
+        url: "https://www.ruanyifeng.com/blog/",
+      },
+    ],
+  },
+  {
+    type: "friends",
+    typeName: "小伙伴们",
+    typeDesc: "我们在一起，共同进步",
+    typeList: [
+      {
+        name: "無名小栈",
+        avatar: "https://pic.efefee.cn/uploads/2024/02/02/65bc526a940e3.png",
+        desc: "分享技术与科技生活",
+        url: "https://blog.imsyy.top/",
+      },
+    ],
+  },
+];
+
+export default toolsData;
